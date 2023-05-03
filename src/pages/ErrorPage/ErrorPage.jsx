60da00a429731e1c713e2a6b0f75fd95d07eda01
Atikur <atikurrahman7223@gmail.com>
2023-05-04 01:15:30 +0600

    added error page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Lottie from "lottie-react";
+import errorImg from '../../assets/error/98642-error-404.json'
+import { Container } from 'react-bootstrap';
+import { Link, useRouteError } from 'react-router-dom';
+import { HiArrowNarrowLeft } from 'react-icons/hi';
+
+const ErrorPage = () => {
+    const {error} = useRouteError();
+    return (
+        <Container className='d-flex align-items-center gap-5'>
+            <Lottie className='w-50' animationData={errorImg} loop={true} />
+            <div>
+                <h2 className='fs-1 fw-bolder'>Page Not Found</h2>
+                <h5 className='my-5'>{error?.message}</h5>
+                <Link to='/' className='my-button fw-bold'> <span className='me-2'> <HiArrowNarrowLeft /> </span> Back to Home page</Link>
+            </div>
+        </Container>
+    );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layouts/Main';
 import Home from '../pages/Home/Home/Home';
 import Blog from '../pages/Blog/Blog';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
